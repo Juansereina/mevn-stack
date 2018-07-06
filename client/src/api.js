0b4a3cd67f2b8fa8d0a039c4a26d153569459e3b
Juansereina <juan-2296@hotmail.es>
2018-07-05 22:33:31 -0500

    api service for client

---
+++ client/src/api.js
@@ -0,0 +1,63 @@
+const axios = require('axios');
+const url = 'http://localhost:5000';
+const getAllTask = () => {
+    try {
+        const response = axios({
+            method: 'get',
+            url: `${url}/task/`
+        });
+        return response;
+    } catch (err) {
+        return err;
+    }
+}
+
+const saveTask = (data) => {
+    try {
+        const response = axios({
+            method: 'post',
+            url: `${url}/task/new`,
+            data
+        });
+        return response;
+    } catch (err) {
+        return err;
+    }
+}
+
+const removeTask = (id) => {
+    try {
+        const response = axios({
+            method: 'delete',
+            url: `${url}/task/${id}`,
+        });
+        return response;
+    } catch (err) {
+        return err;
+    }
+}
+
+const updateTask = ({
+    _id,
+    ...props
+}) => {
+    try {
+        const response = axios({
+            method: 'put',
+            url: `${url}/task/${_id}`,
+            data: {
+                ...props
+            }
+        });
+        return response;
+    } catch (err) {
+        return err;
+    }
+}
+
+export default {
+    getAllTask,
+    saveTask,
+    removeTask,
+    updateTask
+}
