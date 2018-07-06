97deb34efed44b6698a3d4a3394a37576349478c
Juansereina <juan-2296@hotmail.es>
2018-07-05 22:32:55 -0500

    cors

--- server/app.js
+++ server/app.js
@@ -7,15 +7,12 @@ const taskRoutes = require('./routes/task.routes');
 var cors = require('cors')
 
 const start = () => {
-    app.use(
-        process.env.NODE_ENV === "test" ?
-        cors('*') : cors(`${process.env.HOST_URL}`)
-    )
+    app.use(cors('*'));
     app.use(express.static(path.join(__dirname, 'public')));
-    app.use(express.json())
+    app.use(express.json());
     app.use('/task', taskRoutes);
     app.listen(process.env.PORT);
-    return config
+    return config;
 }
 
 module.exports = start;
